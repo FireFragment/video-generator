0bad91f71d538b0410230c5d42fe3a5b4984d0cc
Maty1000 <55660550+Maty1000@users.noreply.github.com>
2020-12-21 17:20:47 +0100

    Removed higher accentutation when word is in uppercase
    Removed higher accentutation when word is in uppercase, because accentutation will be driven only by beat strenght.

--- word.cpp
+++ word.cpp
@@ -4,11 +4,7 @@ void word::changeWord(string textOfWord)
 {
 	text = textOfWord;
 
-	//Checks, if word is in uppercase. If yes, sets accentutation to 3.
-	accentutation = 3;
-	for (size_t i = 0; i < textOfWord.length(); i++)
-		if ((!isupper(static_cast<unsigned char>(textOfWord[i]))) && isalpha(static_cast<unsigned char>(textOfWord[i])))
-			accentutation = 0;
+	
 }
 
 word::word(string textOfWord) {
