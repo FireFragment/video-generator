8b8a7f3b9ee63cf956be2a48a1b8b2bbb0bb1ca8
FireFragment <55660550+FireFragment@users.noreply.github.com>
2021-06-30 20:00:17 +0200

    Cleanup of style class

--- src/style.cpp
+++ src/style.cpp
@@ -3,10 +3,8 @@
 style::style()
 {
 	string normalFontWithPluses = normalFont;
-	string accentFontWithPluses = accentFont;
 	replace(normalFontWithPluses.begin(), normalFontWithPluses.end(), ' ', '+');
-	replace(accentFontWithPluses.begin(), accentFontWithPluses.end(), ' ', '+');
-	baseCSS = "@import url(\"https://fonts.googleapis.com/css2?family=" + normalFontWithPluses + "&family=" + accentFontWithPluses + "&display=swap\");body{overflow:hidden;font-size:2vw;font-family:\"" + normalFont + "\";height:100vh;margin:0;background-color:#000;color:#fff;display:flex;justify-content:center;align-items:center}div{position:absolute;}";
+	baseCSS = "@import url(\"https://fonts.googleapis.com/css2?family=" + normalFontWithPluses + "&display=swap\");body{overflow:hidden;font-size:2vw;font-family:\"" + normalFont + "\";height:100vh;margin:0;background-color:#000;color:#fff;display:flex;justify-content:center;align-items:center}div{position:absolute;}";
 }
 
 string style::getEscapedMusicURL()
--- src/style.h
+++ src/style.h
@@ -11,31 +11,18 @@ class style
 {
 public:
 	style();
-	vector<transitionSet> transitionsSets = { transitionSet() };
 	double speed = 1;
 
 	/// <summary>
 	/// Name of font from Google fonts (https://fonts.google.com/) used by style.
 	/// </summary>
 	string normalFont = "Josefin Sans";
-	string accentFont = "Permanent Marker";
 
 	string baseCSS; // Assigned in constructor.
 	string musicURL;
 
 	/// <returns>Escaped musicURL (all \ replaces with /)</returns>
 	string getEscapedMusicURL();
-
-	/// <summary>
-	/// Relative time. See <see cref="beatGroup::getRelativeTimeAfterBeat" /> for more info about relative times.
-	/// <para>If is this value bigger than relative time to previous beat, addingTransition will be used instead of normalTransition.</para>
-	/// </summary>
-	float maximumRelTimeDistForAddingTrans = 1.5;
-	/// <summary>
-	/// Time in seconds.
-	/// <para>If is this value bigger than time from previous beat, accentingTransition will be used instead of normalTransition.</para>
-	/// </summary>
-	float maximumFreqForAccentingTrans = 0.1;
 	
 	/// <summary>
 	/// How strong animations in the video are.
